05ac1839cf0e3998315b06c1addd2264fb9bd8f3
Fernando Link <fernandolink@gmail.com>
2022-05-19 18:35:47 -0300

    Repetição.

--- estruturas/Estruturas.java
+++ estruturas/Estruturas.java
@@ -6,17 +6,17 @@ public class Estruturas {
     private static final String ESTUDE_MAIS = "Estude mais.";
 
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        float n1 = teclado.nextFloat();
-        float n2 = teclado.nextFloat();
-        float m = (n1+n2)/2;
-        System.out.println("Sua média foi " + m);
-        if (m>9) {
-            System.out.println("Parabéns!");
-        } else {
-            System.out.println(ESTUDE_MAIS);
+        try (Scanner teclado = new Scanner(System.in)) {
+            float n1 = teclado.nextFloat();
+            float n2 = teclado.nextFloat();
+            float m = (n1+n2)/2;
+            System.out.println("Sua média foi " + m);
+            if (m>9) {
+                System.out.println("Parabéns!");
+            } else {
+                System.out.println(ESTUDE_MAIS);
+            }
         }
-
         int idade = 71;
         if (idade < 16) {
             System.out.println("Não vota!");
