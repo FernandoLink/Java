b5b24a7861f135c20753fcaca1fafb675ffc68b3
Flink <flink@v2com.mobi>
2022-06-08 16:41:50 -0300

    Aula 06 encapsulamento

---
+++ poo/aula06/aula06.java
@@ -0,0 +1,14 @@
+package poo.aula06;
+
+public class aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+
+    }
+}
