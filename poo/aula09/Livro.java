6c5992d6fc1eb511ab3d526a6f21396074e87b6b
Flink <flink@v2com.mobi>
2022-06-13 13:53:15 -0300

    Aula 09

---
+++ poo/aula09/Livro.java
@@ -0,0 +1,97 @@
+package poo.aula09;
+
+public class Livro implements Publicacao {
+    private String titulo;
+    private String autor;
+    private int totPaginas;
+    private int pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+
+    public String detalhes() {
+        return "Livro{ titulo=" + titulo + ", autor=" + autor + ", Total de Páginas=" + totPaginas + ", Página Atual="
+                + pagAtual + ", Aberto=" + aberto + ", Leitor=" + leitor.getNome() + " }";
+    }
+
+    public Livro(String titulo, String autor, int totPaginas, Pessoa leitor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.totPaginas = totPaginas;
+        this.pagAtual = 0;
+        this.aberto = false;
+        this.leitor = leitor;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public int getTotPaginas() {
+        return this.totPaginas;
+    }
+
+    public void setPagAtual(int pagAtual) {
+        this.pagAtual = pagAtual;
+    }
+
+    public int getPagAtual() {
+        return this.pagAtual;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public boolean getAberto() {
+        return this.aberto;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+
+    public Pessoa getLeitor() {
+        return this.leitor;
+    }
+
+    @Override
+    public void abrir() {
+        this.aberto = true;
+    }
+
+    @Override
+    public void fechar() {
+        this.aberto = false;
+    }
+
+    @Override
+    public void folhear(int pagina) {
+        this.pagAtual = pagina;
+    }
+
+    @Override
+    public void avancarPag() {
+        this.pagAtual++;
+    }
+
+    @Override
+    public void voltarPag() {
+        this.pagAtual--;
+    }
+}
