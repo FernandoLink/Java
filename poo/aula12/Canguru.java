b44963f4d3e1c7e657e9de261650e47672b19cb0
Flink <flink@v2com.mobi>
2022-06-14 13:43:40 -0300

    Aula 12. sobreposição

---
+++ poo/aula12/Canguru.java
@@ -0,0 +1,10 @@
+public class Canguru extends Mamifero {
+    @Override
+    public void locomover() {
+        System.out.println("Pulando");
+    }
+
+    public void usarBolsar() {
+        System.out.println("Usar bolsa");
+    }
+}
