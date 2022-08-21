becfb88ea76b945fe2e6c676a55776e0a0a4f3e3
Flink <fernandolink@gmail.com>
2022-08-21 15:27:45 -0300

    quarkus

---
+++ quarkus/meu-primeiro-crud/src/main/java/com/github/fernandolink/HelloResources.java
@@ -0,0 +1,16 @@
+package com.github.fernandolink;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResources {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello Meu Primeiro CRUD";
+    }
+}
---
+++ quarkus/meu-primeiro-crud/src/test/java/com/github/fernandolink/HelloResourcesIT.java
@@ -0,0 +1,9 @@
+package com.github.fernandolink;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloResourcesIT extends HelloResourcesTest {
+
+    // Execute the same tests but in native mode.
+}
