1af04f1f0ae52c3701162e81b53244e07074a54d
Flink <fernandolink@gmail.com>
2022-10-26 17:07:24 -0300

    quarkusda

--- quarkus/vintageStore/rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
+++ quarkus/vintageStore/rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
@@ -1,8 +1,11 @@
 package org.agoncal.quarkus.microservices.book;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.time.Instant;
 
 import javax.inject.Inject;
+import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.Produces;
@@ -11,6 +14,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
@@ -25,7 +30,8 @@ public class BookResource {
     @Inject
     Logger logger;
 
-
+    @Retry(delay = 3000, maxRetries = 3)
+    @Fallback(fallbackMethod = "fallbackOnCreatingABook")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
@@ -40,5 +46,26 @@ public class BookResource {
         book.creationDate = Instant.now();
         logger.info("Book created : " + book);
         return Response.status(201).entity(book).build();
+    }
+
+    public Response fallbackOnCreatingABook(String title, String author,
+            int yearOfPublication, String genre) throws FileNotFoundException {
+        Book book = new Book();
+        book.isbn13 = "Will be set later";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+        saveBookOnDisk(book);
+        logger.warn("Book saved on disk: " + book);
+        return Response.status(206).entity(book).build();
+    }
+
+    private void saveBookOnDisk(Book book) throws FileNotFoundException {
+        String bookJson = JsonbBuilder.create().toJson(book);
+        try (PrintWriter out = new PrintWriter("book-" + Instant.now().toEpochMilli() + ".json")){
+            out.println(bookJson);
+        }
     }
 }
---
+++ quarkus/vintageStore/rest-book/src/test/java/org/agoncal/quarkus/microservices/book/MockNumberProxy.java
@@ -0,0 +1,18 @@
+package org.agoncal.quarkus.microservices.book;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import io.quarkus.test.Mock;
+
+@Mock
+@RestClient
+public class MockNumberProxy implements NumberProxy {
+
+    @Override
+    public IsbnThirteen generateIsbnNumber() {
+        IsbnThirteen isbnThirteen = new IsbnThirteen();
+        isbnThirteen.isbn13 = "13-mock";
+        return isbnThirteen;
+    }
+    
+}
