df51b41cb1333a57091171cb6ddc52fb76f7bd1a
Flink <fernandolink@gmail.com>
2022-12-12 20:27:42 -0300

    Modules

--- ecommerce/src/main/java/br/com/link/ecommerce/Order.java
+++ ecommerce/src/main/java/br/com/link/ecommerce/Order.java
@@ -3,13 +3,13 @@ package br.com.link.ecommerce;
 import java.math.BigDecimal;
 
 public class Order {
-	private final String userId, orderId;
-	private final BigDecimal amount;
 
-	public Order(String userId, String orderId, BigDecimal amount) {
-		this.userId = userId;
-		this.orderId = orderId;
-		this.amount = amount;
-	}
+    private final String userId, orderId;
+    private final BigDecimal amount;
 
+    public Order(String userId, String orderId, BigDecimal amount) {
+        this.userId = userId;
+        this.orderId = orderId;
+        this.amount = amount;
+    }
 }
