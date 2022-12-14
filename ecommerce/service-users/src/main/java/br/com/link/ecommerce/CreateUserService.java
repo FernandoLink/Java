308b37ed0874c830da76410280d8a750a52eddd2
Flink <fernandolink@gmail.com>
2022-12-14 18:28:03 -0300

    httpservice kafka

---
+++ ecommerce/service-users/src/main/java/br/com/link/ecommerce/CreateUserService.java
@@ -0,0 +1,64 @@
+package br.com.link.ecommerce;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class CreateUserService {
+
+	private final Connection connection;
+
+	CreateUserService() throws SQLException {
+		String url = "jdbc:sqlite:src/main/resources/users_database.db";
+		this.connection = DriverManager.getConnection(url);
+		try {
+			connection.createStatement()
+					.execute("create table Users (uuid varchar(200) primary key, email varchar(200))");
+		} catch (SQLException e) {
+			// be careful, the sql could be wrong, be really careful
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws SQLException {
+		var userService = new CreateUserService();
+		try (var service = new KafkaService<>(CreateUserService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER",
+				userService::parse, Order.class, Map.of())) {
+			service.run();
+		}
+	}
+
+	private void parse(ConsumerRecord<String, Order> record)
+			throws ExecutionException, InterruptedException, SQLException {
+		System.out.println("------------------------------------------");
+		System.out.println("Processing new order, checking for new user");
+		System.out.println(record.value());
+
+		var order = record.value();
+		if (isNewUser(order.getEmail())) {
+			insertNewUser(order.getEmail());
+		}
+	}
+
+	private void insertNewUser(String email) throws SQLException {
+		var insert = connection.prepareStatement("insert into Users (uuid, email) values (?,?)");
+		insert.setString(1, UUID.randomUUID().toString());
+		insert.setString(2, email);
+		insert.execute();
+		System.out.println("Usuario uuid e " + email + " adicionado");
+
+	}
+
+	private boolean isNewUser(String email) throws SQLException {
+		var exists = connection.prepareStatement("select uuid from Users where email = ? limit 1");
+		exists.setString(1, email);
+		var results = exists.executeQuery();
+		return !results.next();
+	}
+
+}
