9e193f9ba83fbf0a8840bad750e645e0cfe55668
Fernando Link <fernandolink@gmail.com>
2023-01-28 18:51:14 -0300

    jetty

--- gerenciador/src/main/java/br/com/link/gerenciador/servlet/ControllerFilter.java
+++ gerenciador/src/main/java/br/com/link/gerenciador/servlet/ControllerFilter.java
@@ -4,25 +4,32 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.link.gerenciador.action.Action;
 
 //@WebFilter("/entry")
 public class ControllerFilter implements Filter {
 
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
-		
+
 		String paramAction = request.getParameter("action");
 
 		String className = "br.com.link.gerenciador.action." + paramAction;
