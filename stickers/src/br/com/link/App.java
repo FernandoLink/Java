7d5090f6e23c58f701d77ef7d569dad8a2a96067
Fernando Link <fernandolink@gmail.com>
2023-03-29 15:07:10 -0300

    imersão java aula 2

--- stickers/src/br/com/link/App.java
+++ stickers/src/br/com/link/App.java
@@ -1,6 +1,8 @@
 package br.com.link;
 
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -26,10 +28,19 @@ public class App {
 		List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
 		// exibir e manipular os dados
+		var geradora = new GeradorDeFigurinhas();
 		for (Map<String, String> filme : listaDeFilmes) {
-			System.out.println(filme.get("title"));
-			System.out.println(filme.get("image"));
-			System.out.println(filme.get("imDbRating"));
+
+			String urlImagem = filme.get("image");
+			String titulo = filme.get("title");
+			String id = filme.get("id");
+
+			InputStream inputStream = new URL(urlImagem).openStream();
+			String nomeArquivo = id + ".png";
+
+			geradora.cria(inputStream, nomeArquivo);
+
+			System.out.println(titulo);
 			System.out.println();
 		}
 	}
