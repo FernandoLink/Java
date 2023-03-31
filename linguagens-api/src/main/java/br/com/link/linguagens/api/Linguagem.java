5534b815d46a2cb447fd09c23d22b86d47ee40dd
Fernando Link <fernandolink@gmail.com>
2023-03-31 17:52:14 -0300

    imersão alura aula 4

--- linguagens-api/src/main/java/br/com/link/linguagens/api/Linguagem.java
+++ linguagens-api/src/main/java/br/com/link/linguagens/api/Linguagem.java
@@ -1,13 +1,22 @@
 package br.com.link.linguagens.api;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "linguagens")
 public class Linguagem {
+
+	@Id
+	private String id;
 	private String title;
 	private String image;
 	private int ranking;
--- linguagens-api/src/main/java/br/com/link/linguagens/api/controller/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/link/linguagens/api/controller/LinguagemController.java
@@ -2,19 +2,29 @@ package br.com.link.linguagens.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.link.linguagens.api.Linguagem;
+import br.com.link.linguagens.api.LinguagemRepository;
 
 @RestController
 public class LinguagemController {
 
-	private List<Linguagem> linguagens = List.of(new Linguagem("Java", "imagem", 1));
+	@Autowired
+	private LinguagemRepository repositorio;
 
-	@GetMapping(value = "/linguagem-preferida")
-	public String processaLinguagemPreferida() {
-		return "Oi, Spring Web Lombok!";
+	@GetMapping("/linguagens")
+	public List<Linguagem> obterLinguagens() {
+		return repositorio.findAll();
+	}
+
+	@PostMapping("/linguagens")
+	public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+		return repositorio.save(linguagem);
 	}
 
 }
