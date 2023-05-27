137dcf96ea7f3275b6ec295cd4b26f15e6818332
Fernando Link <fernandolink@gmail.com>
2023-05-26 22:14:25 -0300

    kafka

--- ecommerce-kafka/src/main/java/br/com/link/ecommerce/EmailService.java
+++ ecommerce-kafka/src/main/java/br/com/link/ecommerce/EmailService.java
@@ -9,28 +9,28 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-public class FraudDetectorService {
+public class EmailService {
 
 	public static void main(String[] args) {
 		var consumer = new KafkaConsumer<String, String>(properties());
-		consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+		consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
 		while (true) {
 			var records = consumer.poll(Duration.ofMillis(100));
 			if (!records.isEmpty()) {
 				for (var record : records) {
 					System.out.println("----------------------------------");
-					System.out.println("Processing new order, checking for fraud");
+					System.out.println("Send email");
 					System.out.println("KEY: " + record.key());
 					System.out.println("VALUE: " + record.value());
 					System.out.println("PARTITION: " + record.partition());
 					System.out.println("OFFSET: " + record.offset());
 					System.out.println("----------------------------------");
 					try {
-						Thread.sleep(5000);
+						Thread.sleep(1000);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					System.out.println("Order processed");
+					System.out.println("Email sent");
 				}
 			}
 		}
@@ -41,10 +41,9 @@ public class FraudDetectorService {
 		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
 		properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG,
-				FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
-		properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+				EmailService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
 		return properties;
 	}
 }
